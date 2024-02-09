3c35cfe76281a5fdc71752264fff70a4453dea18
Magnus <mp-se@users.noreply.github.com>
2024-02-09 18:55:37 +0100

    Fixed chart loading error in wifi mode

--- src/views/GravityAnalysisView.vue
+++ src/views/GravityAnalysisView.vue
@@ -11,19 +11,12 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { Chart, registerables } from 'chart.js'
+import { ref, onMounted, defineAsyncComponent } from 'vue'
 import { global, config } from "@/modules/pinia"
 
 const chartDataForm = ref([]);
 const chartDataCalc = ref([]);
 const chart = ref(null);
-
-/*try {
-  Chart.register(...registerables);
-} catch( e ) {
-  global.messageError = "Unable to initiate chart.js"
-}*/
 
 const dataSetChart = ref({
   datasets: [{
@@ -110,7 +103,6 @@ onMounted(() => {
     console.log(err)
   }
 })
-// @import "https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"
 </script>
 
 <style>
