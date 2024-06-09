33604b674a1ef060475c955b685a6ad5f3b7ef13
Magnus <mp-se@users.noreply.github.com>
2024-06-09 21:39:06 +0200

    Updated ble settings

--- src/views/PushBluetoothView.vue
+++ src/views/PushBluetoothView.vue
@@ -4,37 +4,45 @@
         <p class="h3">Push - Bluetooth</p>
         <hr>
 
-        <form v-if="status.platform!=='esp8266' && status.platform!=='esp32s2'" @submit.prevent="save" class="needs-validation" novalidate>
+        <template v-if="status.platform !== 'esp8266' && status.platform !== 'esp32s2'">
+            <form @submit.prevent="save" class="needs-validation" novalidate>
+                <div class="row">
+                    <div class="col-md-12">
+                        <BsInputRadio v-model="config.ble_tilt_color" :options="bleTiltColorOptions" label="Tilt color"
+                            help="Tilt color beacon. Only used when tilt type is selected." :disabled="tilt" />
+                    </div>
+                    <div class="col-md-12">
+                        <BsInputRadio v-model="config.ble_format" :options="bleFormatOptions"
+                            label="Bluetooth data format" help="Select the type of bluetooth transmission used."
+                            :disabled="global.disabled32" />
+                    </div>
+                    <div class="col-md-12">
+                        <p></p>
+                        <p>Changing bluetooth settings might require a restart to function properly</p>
+                    </div>
+                </div>
+                <div class="row gy-2">
+                    <div class="col-md-12">
+                        <hr>
+                    </div>
+                    <div class="col-md-3">
+                        <button type="submit" class="btn btn-primary w-2"
+                            :disabled="global.disabled32 || !global.configChanged">
+                            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+                                :hidden="!global.disabled32"></span>
+                            &nbsp;Save
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </template>
+        <template v-else>
             <div class="row">
                 <div class="col-md-12">
-                    <BsInputRadio v-model="config.ble_tilt_color" :options="bleTiltColorOptions" label="Tilt color"
-                        help="Tilt color beacon. Only used when tilt type is selected." :disabled="tilt" />
-                </div>
-                <div class="col-md-12">
-                    <BsInputRadio v-model="config.ble_format" :options="bleFormatOptions" label="Bluetooth data format"
-                        help="Select the type of bluetooth transmission used." :disabled="global.disabled32" />
+                    <p>Bluetooth is not available on this platform</p>
                 </div>
             </div>
-            <div class="row gy-2">
-                <div class="col-md-12">
-                    <hr>
-                </div>
-                <div class="col-md-3">
-                    <button type="submit" class="btn btn-primary w-2" :disabled="global.disabled32 || !global.configChanged">
-                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
-                            :hidden="!global.disabled32"></span>
-                        &nbsp;Save
-                    </button>
-                </div>
-            </div>
-        </form>
-
-        <div class="row">
-            <div class="col-md-12">
-                <p>Bluetooth is not available on this platform</p>
-            </div>
-        </div>
-
+        </template>
     </div>
 </template>
 
@@ -75,8 +83,12 @@ const tilt = computed(() => {
 })
 
 const save = () => {
+    if((config.ble_format == 1 || config.ble_format == 2) && config.ble_tilt_color == "") {
+        config.ble_tilt_color = "red"
+    }
+
     if (!validateCurrentForm()) return
-    
+
     global.clearMessages()
     config.saveAll()
 }
