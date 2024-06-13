435a203f598a92c30883640065efaf5ad35a657c
Magnus <mp-se@noreply.github.com>
2024-06-13 07:34:25 +0200

    Refactor

--- src/modules/configStore.js
+++ src/modules/configStore.js
@@ -92,15 +92,15 @@ export const useConfigStore = defineStore('config', {
             var dest = {}
 
             for (var key in this.$state) {
-                if( !key.startsWith("$")) {
-                    if(key === "gyro_calibration_data") {
+                if (!key.startsWith("$")) {
+                    if (key === "gyro_calibration_data") {
                         dest[key] = []
-                        for(var i in this.$state[key]) {                         
+                        for (var i in this.$state[key]) {
                             dest[key][i] = this.$state[key][i]
                         }
-                    } else if(key === "formula_calculation_data") {
+                    } else if (key === "formula_calculation_data") {
                         dest[key] = []
-                        for(var i in this.$state[key]) {          
+                        for (var i in this.$state[key]) {
                             dest[key][i] = {}
                             dest[key][i].a = this.$state[key][i].a
                             dest[key][i].g = this.$state[key][i].g
@@ -126,82 +126,82 @@ export const useConfigStore = defineStore('config', {
                 .then(json => {
                     logDebug("configStore.load()", json)
                     global.disabled = false
-                    this.id = json.id,
-                        // Device
-                        this.mdns = json.mdns,
-                        this.temp_format = json.temp_format,
-                        this.gravity_format = json.gravity_format,
-                        // Hardware
-                        this.ota_url = json.ota_url,
-                        this.storage_sleep = json.storage_sleep,
-                        this.voltage_factor = json.voltage_factor,
-                        this.voltage_config = json.voltage_config,
-                        this.gyro_temp = json.gyro_temp,
-                        this.battery_saving = json.battery_saving,
-                        this.tempsensor_resolution = json.tempsensor_resolution,
-                        this.temp_adjustment_value = json.temp_adjustment_value,
-                        this.gyro_disabled = json.gyro_disabled,
-                        this.voltage_pin = json.voltage_pin,
-                        // Wifi
-                        this.wifi_portal_timeout = json.wifi_portal_timeout,
-                        this.wifi_connect_timeout = json.wifi_connect_timeout,
-                        this.wifi_ssid = json.wifi_ssid,
-                        this.wifi_ssid2 = json.wifi_ssid2,
-                        this.wifi_pass = json.wifi_pass,
-                        this.wifi_pass2 = json.wifi_pass2,
-                        // Push - Generic
-                        this.token = json.token,
-                        this.token2 = json.token2,
-                        this.sleep_interval = json.sleep_interval,
-                        this.push_timeout = json.push_timeout,
-                        this.skip_ssl_on_test = json.skip_ssl_on_test,
-                        // Push - Http Post 1
-                        this.http_post_target = json.http_post_target
-                        this.http_post_header1 = json.http_post_header1
-                        this.http_post_header2 = json.http_post_header2
-                        this.http_post_int = json.http_post_int,
-                        this.http_post_format = json.http_post_format,
-                        // Push - Http Post 2
-                        this.http_post2_target = json.http_post2_target
-                        this.http_post2_header1 = json.http_post2_header1
-                        this.http_post2_header2 = json.http_post2_header2
-                        this.http_post2_int = json.http_post2_int,
-                        this.http_post2_format = json.http_post2_format,
-                        // Push - Http Get
-                        this.http_get_target = json.http_get_target
-                        this.http_get_header1 = json.http_get_header1
-                        this.http_get_header2 = json.http_get_header2
-                        this.http_get_int = json.http_get_int,
-                        this.http_get_format = json.http_get_format,
-                        // Push - Influx
-                        this.influxdb2_target = json.influxdb2_target,
-                        this.influxdb2_org = json.influxdb2_org,
-                        this.influxdb2_bucket = json.influxdb2_bucket,
-                        this.influxdb2_token = json.influxdb2_token,
-                        this.influxdb2_int = json.influxdb2_int,
-                        this.influxdb2_format = json.influxdb2_format,
-                        // Push - MQTT
-                        this.mqtt_target = json.mqtt_target,
-                        this.mqtt_port = json.mqtt_port,
-                        this.mqtt_user = json.mqtt_user,
-                        this.mqtt_pass = json.mqtt_pass,
-                        this.mqtt_int = json.mqtt_int,
-                        this.mqtt_format = json.mqtt_format,
-                        // Push BLE
-                        this.ble_tilt_color = json.ble_tilt_color,
-                        this.ble_format = json.ble_format,
-                        // Gravity formula
-                        this.gravity_formula = json.gravity_formula,
-                        this.gravity_temp_adjustment = json.gravity_temp_adjustment,
-                        this.gyro_read_count = json.gyro_read_count,
-                        this.gyro_moving_threashold = json.gyro_moving_threashold,
-                        this.formula_max_deviation = json.formula_max_deviation,
-                        this.formula_calibration_temp = json.formula_calibration_temp,
-                        this.ignore_low_angles = json.ignore_low_angles,
-                        this.formula_calculation_data = json.formula_calculation_data,
-                        this.gyro_calibration_data = json.gyro_calibration_data,
-                        this.dark_mode = json.dark_mode,
-                        callback(true)
+                    this.id = json.id
+                    // Device
+                    this.mdns = json.mdns
+                    this.temp_format = json.temp_format
+                    this.gravity_format = json.gravity_format
+                    // Hardware
+                    this.ota_url = json.ota_url
+                    this.storage_sleep = json.storage_sleep
+                    this.voltage_factor = json.voltage_factor
+                    this.voltage_config = json.voltage_config
+                    this.gyro_temp = json.gyro_temp
+                    this.battery_saving = json.battery_saving
+                    this.tempsensor_resolution = json.tempsensor_resolution
+                    this.temp_adjustment_value = json.temp_adjustment_value
+                    this.gyro_disabled = json.gyro_disabled
+                    this.voltage_pin = json.voltage_pin
+                    // Wifi
+                    this.wifi_portal_timeout = json.wifi_portal_timeout
+                    this.wifi_connect_timeout = json.wifi_connect_timeout
+                    this.wifi_ssid = json.wifi_ssid
+                    this.wifi_ssid2 = json.wifi_ssid2
+                    this.wifi_pass = json.wifi_pass
+                    this.wifi_pass2 = json.wifi_pass2
+                    // Push - Generic
+                    this.token = json.token
+                    this.token2 = json.token2
+                    this.sleep_interval = json.sleep_interval
+                    this.push_timeout = json.push_timeout
+                    this.skip_ssl_on_test = json.skip_ssl_on_test
+                    // Push - Http Post 1
+                    this.http_post_target = json.http_post_target
+                    this.http_post_header1 = json.http_post_header1
+                    this.http_post_header2 = json.http_post_header2
+                    this.http_post_int = json.http_post_int
+                    this.http_post_format = json.http_post_format
+                    // Push - Http Post 2
+                    this.http_post2_target = json.http_post2_target
+                    this.http_post2_header1 = json.http_post2_header1
+                    this.http_post2_header2 = json.http_post2_header2
+                    this.http_post2_int = json.http_post2_int
+                    this.http_post2_format = json.http_post2_format
+                    // Push - Http Get
+                    this.http_get_target = json.http_get_target
+                    this.http_get_header1 = json.http_get_header1
+                    this.http_get_header2 = json.http_get_header2
+                    this.http_get_int = json.http_get_int
+                    this.http_get_format = json.http_get_format
+                    // Push - Influx
+                    this.influxdb2_target = json.influxdb2_target
+                    this.influxdb2_org = json.influxdb2_org
+                    this.influxdb2_bucket = json.influxdb2_bucket
+                    this.influxdb2_token = json.influxdb2_token
+                    this.influxdb2_int = json.influxdb2_int
+                    this.influxdb2_format = json.influxdb2_format
+                    // Push - MQTT
+                    this.mqtt_target = json.mqtt_target
+                    this.mqtt_port = json.mqtt_port
+                    this.mqtt_user = json.mqtt_user
+                    this.mqtt_pass = json.mqtt_pass
+                    this.mqtt_int = json.mqtt_int
+                    this.mqtt_format = json.mqtt_format
+                    // Push BLE
+                    this.ble_tilt_color = json.ble_tilt_color
+                    this.ble_format = json.ble_format
+                    // Gravity formula
+                    this.gravity_formula = json.gravity_formula
+                    this.gravity_temp_adjustment = json.gravity_temp_adjustment
+                    this.gyro_read_count = json.gyro_read_count
+                    this.gyro_moving_threashold = json.gyro_moving_threashold
+                    this.formula_max_deviation = json.formula_max_deviation
+                    this.formula_calibration_temp = json.formula_calibration_temp
+                    this.ignore_low_angles = json.ignore_low_angles
+                    this.formula_calculation_data = json.formula_calculation_data
+                    this.gyro_calibration_data = json.gyro_calibration_data
+                    this.dark_mode = json.dark_mode
+                    callback(true)
                 })
                 .catch(err => {
                     global.disabled = false
@@ -290,29 +290,29 @@ export const useConfigStore = defineStore('config', {
 
             data = data2.http_post_format !== undefined ? { http_post_format: encodeURIComponent(data2.http_post_format) } : {}
             this.sendOneFormat(data, (success) => {
-                if(success) cnt += 1
+                if (success) cnt += 1
                 data = data2.http_post2_format !== undefined ? { http_post2_format: encodeURIComponent(data2.http_post2_format) } : {}
                 this.sendOneFormat(data, (success) => {
-                    if(success) cnt += 1
+                    if (success) cnt += 1
                     data = data2.http_get_format !== undefined ? { http_get_format: encodeURIComponent(data2.http_get_format) } : {}
                     this.sendOneFormat(data, (success) => {
-                        if(success) cnt += 1
+                        if (success) cnt += 1
                         data = data2.influxdb2_format !== undefined ? { influxdb2_format: encodeURIComponent(data2.influxdb2_format) } : {}
                         this.sendOneFormat(data, (success) => {
-                            if(success) cnt += 1
+                            if (success) cnt += 1
 
-                            if(data2.mqtt_format !== undefined) {
+                            if (data2.mqtt_format !== undefined) {
                                 data2.mqtt_format = data2.mqtt_format.replaceAll("\n", "")
                                 data2.mqtt_format = data2.mqtt_format.replaceAll("\r", "")
                             }
 
-                            data = data2.mqtt_format !== undefined ? { mqtt_format: encodeURIComponent(data2.mqtt_format) } : {}   
+                            data = data2.mqtt_format !== undefined ? { mqtt_format: encodeURIComponent(data2.mqtt_format) } : {}
                             this.sendOneFormat(data, (success) => {
-                                if(success) cnt += 1
+                                if (success) cnt += 1
 
-                                if(cnt == 5)
+                                if (cnt == 5)
                                     callback(true)
-                                else 
+                                else
                                     callback(false)
                             })
                         })
@@ -412,8 +412,8 @@ export const useConfigStore = defineStore('config', {
         },
         getWifiScanStatus(callback) {
             logInfo("configStore.getWifiScanStatus()", "Fetching /api/wifi/status")
-            fetch(global.baseURL + 'api/wifi/status', { 
-                method: "GET", 
+            fetch(global.baseURL + 'api/wifi/status', {
+                method: "GET",
                 headers: { "Authorization": global.token },
                 signal: AbortSignal.timeout(global.fetchTimout),
             })
@@ -451,8 +451,8 @@ export const useConfigStore = defineStore('config', {
         },
         getHardwareScanStatus(callback) {
             logInfo("configStore.getHardwareScanStatus()", "Fetching /api/hardware/status")
-            fetch(global.baseURL + 'api/hardware/status', { 
-                method: "GET", 
+            fetch(global.baseURL + 'api/hardware/status', {
+                method: "GET",
                 headers: { "Authorization": global.token },
                 signal: AbortSignal.timeout(global.fetchTimout),
             })
