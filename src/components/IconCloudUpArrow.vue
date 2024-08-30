f7dd3812e6e3666388fbcc2ebe9147ccdca91610
Magnus <mp-se@noreply.github.com>
2024-08-30 16:08:48 +0200

    Refactor Icons

---
+++ src/components/IconCloudUpArrow.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg v-bind="$attrs" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+    <path
+      fill-rule="evenodd"
+      d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708z"
+    />
+  </svg>
+</template>
+
+<script setup>
+/**
+ * 2024-05-28 Bootstrap VueJS wrapper, Magnus Persson
+ */
+
+/**
+ * Purpose: Show an svg icon on the UI. Used embedded svg to avoid downloading data from internet.
+ */
+defineOptions({
+  inheritAttrs: false
+})
+</script>
